79f362ffabfa67e2287c8d9b750fb27e1b92c92e
veighnsche <vincepaul.liem@gmail.com>
2025-04-04 14:07:59 +0200

    Update ErrorHandlerDirector to use ErrorConduit instead of AppConduit
    Replaced AppConduit with the newly introduced ErrorConduit in ErrorHandlerDirector to better handle C8Error contexts. Adjusted the ErrorConduit implementation to ensure compatibility with C8Error and maintain consistency with VHXRedprint interface.

--- src/_stage/conduits/ErrorConduit.ts
+++ src/_stage/conduits/ErrorConduit.ts
@@ -1,5 +1,4 @@
 // src/_stage/conduits/AppConduit.ts
-import { Context } from 'hono';
 import { CoreRedprint, StrictObjectKVService } from '../../_core';
 import { C8Error } from '../../_core/Recorder/C8Error';
 import { VHXRedprint } from '../actors/vhx';
@@ -7,7 +6,7 @@ import { LifeCycleService } from '../services/LifeCycleService';
 import { VHXService } from '../services/VhxService';
 import { AppConduit } from './AppConduit';
 
-export class ErrorConduit extends CoreRedprint<C8Error<AppConduit>> implements VHXRedprint<Context> {
+export class ErrorConduit extends CoreRedprint<C8Error<AppConduit>> implements VHXRedprint<C8Error<AppConduit>> {
 	public locals = new StrictObjectKVService('locals');
 	public vhx = new VHXService('vhx');
 
--- src/directors/error-handler.tsx
+++ src/directors/error-handler.tsx
@@ -1,7 +1,8 @@
 import { createDirector } from '../_core';
 import { C8Error } from '../_core/Recorder/C8Error';
 import { AppConduit } from '../_stage/conduits/AppConduit';
+import { ErrorConduit } from '../_stage/conduits/ErrorConduit';
 
 const ErrorHandlerDirector = createDirector('Error Handler Director').init<C8Error<AppConduit>>(error => ({
-	conduit: new AppConduit(error),
+	conduit: new ErrorConduit(error),
 }));
